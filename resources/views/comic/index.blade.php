270f7f6175f99738af5f88781aa3cccb4354d7b8
giorgios17 <giosur@live.it>
2022-05-25 15:25:53 +0200

    creato layout e implementato nell'index

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="css/app.css">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics</title>
-</head>
-
-<body>
+@extends('layout.app')
+@section('content')
     @foreach ($comics as $comic)
         <div class="card">
             <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
@@ -31,6 +17,4 @@
             <p>{{ $comic['type'] }}</p>
         </div>
     @endforeach
-</body>
-
-</html>
+@endsection()
