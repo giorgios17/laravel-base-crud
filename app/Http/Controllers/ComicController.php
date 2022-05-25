7a607460731c8fc42220c7384fffd518781f40e8
giorgios17 <giosur@live.it>
2022-05-25 14:02:12 +0200

    collegato controller alla route

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -15,6 +15,7 @@ class ComicController extends Controller
     public function index()
     {
         //
+        return view('home', config('comics'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'ComicController@index');
 
 Route::resource('comics', UserController::class);
