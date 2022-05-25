c8227d001958371be2177b3db82d67c296c26e5f
giorgios17 <giosur@live.it>
2022-05-25 16:34:55 +0200

    stile

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.app')
 @section('content')
-    <div>
+    <div class="card margin-auto">
+        <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
         <h3>Titolo: </h3>
         <p>{{ $comic->title }}</p>
         <h3>Descrizione:</h3>
@@ -12,6 +13,6 @@
         <p>{{ $comic->sale_date }}</p>
         <h3>Genere: </h3>
         <p>{{ $comic->type }}</p>
+        <a href="{{ route('comics.index') }}">Torna alla lista fumetti</a>
     </div>
-    <a href="{{ route('comics.index') }}">Lista fumetti</a>
 @endsection
